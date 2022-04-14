ff9c1741148de86647791b280b6851f37e3afa30
Rumon360 <hmk.rumon@gmail.com>
2022-04-15 03:10:59 +0600

    fixed ui

--- src/js/Home.js
+++ src/js/Home.js
@@ -22,4 +22,28 @@ const hamburgerMenu = () => {
   }
 };
 
+const checkboxOne = (e) => {
+  if (e.target.checked) {
+    document.querySelector(".checkboxOne").classList.add("bg-blue-400");
+  } else {
+    document.querySelector(".checkboxOne").classList.remove("bg-blue-400");
+  }
+};
+
+const checkboxTwo = (e) => {
+  if (e.target.checked) {
+    document.querySelector(".checkboxTwo").classList.add("bg-blue-400");
+  } else {
+    document.querySelector(".checkboxTwo").classList.remove("bg-blue-400");
+  }
+};
+
+const checkboxThree = (e) => {
+  if (e.target.checked) {
+    document.querySelector(".checkboxThree").classList.add("bg-blue-400");
+  } else {
+    document.querySelector(".checkboxThree").classList.remove("bg-blue-400");
+  }
+};
+
 const getData = () => {};
